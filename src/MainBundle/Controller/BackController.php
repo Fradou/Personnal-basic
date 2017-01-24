0904ea5a215c07b4f71d00ae98e1c208369a8768
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-01-24 23:54:36 +0100

    Able to get number of column and label but labels contain BundleName\Entity, need to clean it up.

--- src/MainBundle/Controller/BackController.php
+++ src/MainBundle/Controller/BackController.php
@@ -3,25 +3,30 @@
 namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use ChasseBundle\Repository\JobRepository;
+use MainBundle\Repository\UserRepository;
 
 class BackController extends Controller
 {
     public function indexAction()
     {
         $entity = $this->getDoctrine()->getRepository('MainBundle:User')->findAll();
+        $entitarray = (array)$entity[0];
 
-        $colnb = count($entity[0]);
+        $colnb = count($entitarray);
         $collab = [];
-        foreach($entity[0] as $key => $value){
+        foreach($entitarray as $key => $value){
             $collab[]=$key;
+        }
+
+        foreach($collab as $key => $value){
+
         }
 
 
         return $this->render('back/template.html.twig', array(
             'colnb' => $colnb,
             'collab' => $collab,
-            'entity' => $entity,
+            'entity' => $entitarray,
             // ...
         ));
     }
