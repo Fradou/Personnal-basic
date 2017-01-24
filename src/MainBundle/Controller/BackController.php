9ef903053ffc533b102210fcafaa94fa45cef331
Alexandre FRADET <alex.fradet.1@gmail.com>
2017-01-24 23:29:14 +0100

    Create view and making method to detect number of field and their label in any entity

--- src/MainBundle/Controller/BackController.php
+++ src/MainBundle/Controller/BackController.php
@@ -3,12 +3,25 @@
 namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ChasseBundle\Repository\JobRepository;
 
 class BackController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MainBundle:Back:index.html.twig', array(
+        $entity = $this->getDoctrine()->getRepository('MainBundle:User')->findAll();
+
+        $colnb = count($entity[0]);
+        $collab = [];
+        foreach($entity[0] as $key => $value){
+            $collab[]=$key;
+        }
+
+
+        return $this->render('back/template.html.twig', array(
+            'colnb' => $colnb,
+            'collab' => $collab,
+            'entity' => $entity,
             // ...
         ));
     }
